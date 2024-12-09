b70ec363026379579d350cef5a5793b52340d468
ABDULBASITKHAN001 <[basitkhurramkhan@gmail.com]>
2024-12-09 13:37:39 +0500

    Chap No.35-38

--- script.js
+++ script.js
@@ -1,39 +1,39 @@
-// console.log("HELLO-WORLD");
-// console.log("")
-// console.log("-------------------------------------------------------------------------------------------")
-// console.log("QUESTION NUMBER 01:")
-// // Create a new Date object for the current date and time
-//  var currentDate = new Date();
-// console.log(currentDate.toDateString());
+console.log("HELLO-WORLD");
+console.log("")
+console.log("-------------------------------------------------------------------------------------------")
+console.log("QUESTION NUMBER 01:")
+// Create a new Date object for the current date and time
+ var currentDate = new Date();
+console.log(currentDate.toDateString());
 
-// console.log("")
-// console.log("----------------------------------------------------------------------------------------------------");
-// console.log("QUESTION NUMBER 02:");
-// // Function to greet the user with their full name using prompt
-// function greetUser() {
-//     var firstName = prompt("Enter your first name:");
-//     var lastName = prompt("Enter your last name:");    
-//     var fullName = firstName + " " + lastName;  
-//     console.log("Hello, " + fullName + "!");    
-// }
-// greetUser();
+console.log("")
+console.log("----------------------------------------------------------------------------------------------------");
+console.log("QUESTION NUMBER 02:");
+// Function to greet the user with their full name using prompt
+function greetUser() {
+    var firstName = prompt("Enter your first name:");
+    var lastName = prompt("Enter your last name:");    
+    var fullName = firstName + " " + lastName;  
+    console.log("Hello, " + fullName + "!");    
+}
+greetUser();
 
 
-// console.log("")
-// console.log("----------------------------------------------------------------------------------------------------")
-// console.log("QUESTION NUMBER 03:");
-// // Function to add two numbers input by the user
-// function addTwoNumbers() {
+console.log("")
+console.log("----------------------------------------------------------------------------------------------------")
+console.log("QUESTION NUMBER 03:");
+// Function to add two numbers input by the user
+function addTwoNumbers() {
   
-//     var num1 = Number(prompt("Enter the first number:"));
-//     var num2 = Number(prompt("Enter the second number:"));
+    var num1 = Number(prompt("Enter the first number:"));
+    var num2 = Number(prompt("Enter the second number:"));
   
-//     var sum = num1 + num2;
+    var sum = num1 + num2;
     
-//     console.log("The sum of the two numbers is: " + sum);
-//     return sum;
-// }
-// addTwoNumbers();
+    console.log("The sum of the two numbers is: " + sum);
+    return sum;
+}
+addTwoNumbers();
 
 console.log("")
 console.log("------------------------------------------------------------------------------------------------------")
