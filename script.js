24fc2f7f21b9f981400e4136f6e921955b1f69ed
ABDULBASITKHAN001 <[basitkhurramkhan@gmail.com]>
2024-12-10 15:11:32 +0500

    Chap No.35-38

--- script.js
+++ script.js
@@ -69,3 +69,92 @@ var num2 = Number(prompt("Enter the second number:"));
 
 
 calculator(num1, num2, operator);
+
+
+console.log("");
+console.log("------------------------------------------------------------------------------------------");
+console.log("QUESTION NUMBER 05:");
+function square(x) {
+    console.log(x * x);
+}
+
+square(4);  
+
+
+console.log("");
+console.log("-------------------------------------------------------------------------------------------------")
+console.log("QUESTION NUMBER 06:");
+function factorial(n) {
+    if (n === 0 || n === 1) {
+        return 1;  
+    } else {
+        return n * factorial(n - 1);  
+    }
+}
+
+console.log(factorial(5));
+
+
+console.log("")
+console.log("------------------------------------------------------------------------------------------------------")
+console.log("QURSTION NUMBER 07:");
+function displayCounting(start, end) {
+    for (var i = start; i <= end; i++) {
+        console.log(i);
+    }
+}
+displayCounting(1, 10);  
+
+
+console.log("")
+console.log("------------------------------------------------------------------------------------------------------");
+console.log("QUESTION NUMBER 08:");
+function capitalizeFirstLetterOfEachWord(str) {
+    var originalString = str; 
+    var words = str.split(' ');  
+    var capitalizedWords = words.map(function(word) {
+        return word.charAt(0).toUpperCase() + word.slice(1);  
+    });
+    var changedString = capitalizedWords.join(' '); 
+    console.log("Original String: " + originalString);
+    console.log("Changed String: " + changedString);
+    
+    return changedString; 
+}
+
+var result = capitalizeFirstLetterOfEachWord('the quick brown fox');
+
+console.log("")
+console.log("------------------------------------------------------------------------------------------------------");
+console.log("QUESTION NUMBER 09:");
+function findLongestWord(str) {
+    var originalString = str; 
+    var words = str.split(' ');
+    var longestWord = '';
+    
+    // Loop through the words and find the longest one
+    for (var i = 0; i < words.length; i++) {
+        if (words[i].length > longestWord.length) {
+            longestWord = words[i]
+        }
+    }
+    console.log("Original String: " + originalString);
+    console.log("Longest Word: " + longestWord);
+    
+    return longestWord;
+}
+
+var result = findLongestWord('Web Development Tutorial');
+
+
+console.log("");
+console.log("------------------------------------------------------------------------------------------------------");
+console.log("QUESTION NUMBER 10:");
+function calculateArea(width, height) {
+    return width * height;
+  }
+  var w = 5;  
+  var h = 10; 
+  var area = calculateArea(w, h);
+  console.log("Area (using variables):", area);
+  
